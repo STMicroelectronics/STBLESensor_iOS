2e9ac2ab3dd4312f8937f9877441490ef194d8df
Darren Pottinger <darren.pottinger@gmail.com>
2019-03-25 21:01:00 -0700

    Update setUpDependency.sh
    Fixes small typos

--- setUpDependency.sh
+++ setUpDependency.sh
@@ -1,17 +1,17 @@
-#to install cacoapods: https://guides.cocoapods.org/using/getting-started.html
+#to install cocoapods: https://guides.cocoapods.org/using/getting-started.html
 pod update
 pod install
 
-#to install carhage:
+#to install carthage:
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"​
 # brew update
 # brew install carthage
-#to update cartage
-# brew update cartage
-echo "Carthage: remove chache and previous build"
+# to update carthage
+# brew update carthage
+echo "Carthage: remove cache and previous build"
 rm -rf ~/Library/Caches/org.carthage.CarthageKit
 rm -rf ./Carthage/Build/
-echo "Carthage: build dependecy"
+echo "Carthage: build dependency"
 carthage update --no-use-binaries --platform iOS
 echo "CHANGE BUNDLE ID!!!!"
 echo "in W2STAPP->BuildSettings-> change APP_VERSION and APP_BUILD"
